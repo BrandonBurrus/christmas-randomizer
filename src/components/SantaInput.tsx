5b4d9f36e8a92734b362ce91ad4f12fc047cdadb
Juan Palomino <jpalomino09@gmail.com>
2020-09-25 16:46:44 -0700

    chore: add css reset and initial layout component

---
+++ src/components/SantaInput.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { InputGroup, Input, Button, InputRightElement } from '@chakra-ui/core';
+
+const SantaInput = () => {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <InputGroup size='md'>
+      <Input pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Enter password' />
+      <InputRightElement width='4.5rem'>
+        <Button h='1.75rem' size='sm' onClick={handleClick}>
+          {show ? 'Hide' : 'Show'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
+
+export default SantaInput;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 function HomePage() {
-  return <h1>Christmas List</h1>;
+  return <Layout />;
 }
 
 export default HomePage;
