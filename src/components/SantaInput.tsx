81cda1d810466ef93bf96bac44f2c75a8d055a1e
Juan Palomino <jpalomino09@gmail.com>
2020-09-25 17:00:38 -0700

    fix: respond to pr comments

--- src/components/SantaInput.tsx
+++ src/components/SantaInput.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputGroup, Input, Button, InputRightElement } from '@chakra-ui/core';
 
 //TODO: this will be the individual component that is used to add the new participants to the list
 function SantaInput() {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
+  const [show, setShow] = useState(false);
 
   return (
     <InputGroup size='md'>
       <Input pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Enter password' />
       <InputRightElement width='4.5rem'>
-        <Button h='1.75rem' size='sm' onClick={handleClick}>
+        <Button h='1.75rem' size='sm' onClick={() => setShow(!show)}>
           {show ? 'Hide' : 'Show'}
         </Button>
       </InputRightElement>
